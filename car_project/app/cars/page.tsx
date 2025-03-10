854a7adbf207775a35af8aa070706b0a65e153cc
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2025-03-10 13:14:08 +0100

    Video 3

--- car_project/app/cars/page.tsx
+++ car_project/app/cars/page.tsx
@@ -4,23 +4,44 @@ import { Car } from '@/lib/types/types'
 
 type Props = {}
 
-const car:Car = {
-    name: "Super Car",
-    type: "BMW X3",
-    color: "red",
-    motor: {
-        serialNumber: 1234,
-        hp: 200,
+const cars: Car[] = [
+    {
+        name: "Super Car",
+        type: "BMW X3",
+        color: "red",
+        motor: {
+            serialNumber: 1234,
+            hp: 200,
+        }
+    },
+
+    {
+        name: "Mega Car",
+        type: "Fiat 500",
+        color: "blue",
+        motor: {
+            serialNumber: 1234,
+            hp: 200,
+        }
+    },
+
+    {
+        name: "Ultra car",
+        type: "passat",
+        color: "pink",
+        motor: {
+            serialNumber: 1234,
+            hp: 200,
+        }
     }
-}
+]
+
 
 export default function Cars({ }: Props) {
     return (
         <div>
             <h1>Cars</h1>
-            <Card car={car} />
-            <Card car={car} />
-            <Card car={car} />
+            {cars.map((car : Car) => (<Card car={car}/>))}
         </div>
     )
 }
--- car_project/app/components/ui/car/card.tsx
+++ car_project/app/components/ui/car/card.tsx
@@ -6,7 +6,11 @@ type Props = { car: Car }
 export default function Card({ car }: Props) {
     return (
         <div className='bg-green-300 border-2 p-4'>
-            Card:{car.name}
+            <h2>Card:{car.name}</h2>
+            <h2>Card:{car.type}</h2>
+            <h2>Card:{car.color}</h2>
+            <h2>Card:{car.motor.serialNumber}</h2>
+            <h2>Card:{car.motor.hp}</h2>
         </div>
     )
 }
